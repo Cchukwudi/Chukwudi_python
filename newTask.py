ae522b2cc9c97bd1928a601e44ca078e1a7362a9
Chukwudi <cchinwenmeri@gmail.com>
2020-03-24 22:44:33 +0100

    Add files via upload

---
+++ newTask.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 22:38:10 2020
+
+@author: HP
+"""
+
+import math
+
+r=float(input('Enter the radius of the cirlce:'))
+
+area=(math.pi*r*2)
+
+print("Area of the circle is: %2f"%area)
